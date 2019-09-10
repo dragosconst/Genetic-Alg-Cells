c96741b7639d03521fe764f6ad794d724365dfd8
Dragos Constantin <tantarudragos@yahoo.com>
2019-09-10 17:02:40 +0300

    wrote the rect methods + standardised some more code
    ok, what I've done in this commit is:
    - renamed the square methods in order to capture their general character, since they can both be used for squares and rectangles, I've made only very slight changes to the functions;
    - some variables in the cell class that should be protected\private were public, so I changed that

--- Machine Learning Cells/Cell/Cell.py
+++ Machine Learning Cells/Cell/Cell.py
@@ -16,17 +16,17 @@ class Cell():
     def __init__(self, app):
         super().__init__()
         self.area = rand.randrange(50000, 100001) # selects a random area for the cell
-        self.sides = [0, 0] # the value for the width and height(they are equivalent for squares and rhombuses(?))
+        self.sides = [0, 0] # the value for the width and height(they are equivalent for squares and rhombuses(? hope this is the plural))
         self.angle = rand.randrange(30, 161) # this is only relevant for the rhombus
         self.baseShapeKey = self._chooseShape() # this is the shape from whitch the cell is drawn
         self._multiplier = np.random.uniform(low = 0.37, high = 0.8, size = (1,)) # this multiplies is used for determing the height(second side) of the rectangle; given the way the paint event works, if the random multiplier were to be calculated in the draw function, it would keep getting change at every call and make a very chaotic drawing
-        self.baseShape = None # the base shape of the cell
-        self.circles = [] # here will be saved the circles drawn on the cell
+        self._baseShape = None # the base shape of the cell
+        self._circles = [] # here will be saved the circles drawn on the cell
         self._circNum = self._chooseNumberOfCircles() # chooses whether to draw one or two circles(and their symmetric counterparts)
         self._sidesWithCircles = [] # similar to the older turtle algorithm, it won't draw two circles on the same side
         
-        self.baseShapeKey = 1
-        self.circNum = 2
+        self.baseShapeKey = 2 # these two values are for testing only, once I finish the drawing algorithm, they will be deleted
+        self._circNum = 2
         self._drawShape(app.mapScene, app.mapGView) # draws the base shape
 
         self._prepCircles(0, 0, app) # this function prepares the circles for being drawn and adds them to the scene, after they are prepped
@@ -83,7 +83,6 @@ class Cell():
     def _prepCircles(self, sideLen, soFar, app):
         if soFar >= self._circNum: # if there have been added enough circles, stop the function
             return 
-
         self._chooseSide() # pretty obvious, chooses a side
 
         coords = self._chooseCoords(self._sidesWithCircles[sideLen])
@@ -91,8 +90,8 @@ class Cell():
         self._resetCoords(None, None, None, None, self._sidesWithCircles[sideLen], radius, coords)
         
         circle, opCircle = self._makeCircle(self._sidesWithCircles[sideLen], coords[0], coords[1], coords[2], coords[3], radius)
-        self.circles.append(circle) # the code should've been pretty straightforward so far
-        self.circles.append(opCircle)
+        self._circles.append(circle) # the code should've been pretty straightforward so far
+        self._circles.append(opCircle)
         app.mapScene.addItem(circle)
         app.mapScene.addItem(opCircle)
 
@@ -100,68 +99,53 @@ class Cell():
 
     # this function works as a nice wrapper for the actual chooseRadius functions
     def _chooseRadius(self, side, sidex, sidey, opsidex, opsidey):
-        if self.baseShapeKey == BaseShape.Square.value:
-            return self._chooseRadiusSquare(side, sidex, sidey, opsidex, opsidey)
+        if self.baseShapeKey == BaseShape.Square.value or self.baseShapeKey == BaseShape.Rect.value: # in this case, the function is literally identical
+            return self._chooseRadiusSquareOrRect(side, sidex, sidey, opsidex, opsidey)
 
     # this is a wrapper for resetCoords functions
     def _resetCoords(self, sidex, sidey, opsidex, opsidey, side, radius, coords = None):
-        if self.baseShapeKey == BaseShape.Square.value:
-            self._resetCoordsSquare(sidex, sidey, opsidex, opsidey, side, radius, coords)
+        if self.baseShapeKey == BaseShape.Square.value or self.baseShapeKey == BaseShape.Rect.value:
+            self._resetCoordsSquareOrRect(sidex, sidey, opsidex, opsidey, side, radius, coords)
 
 
     # FUNCTIONS FOR CHOOSING THE COORDS AND THE RADIUS FOR THE SEMI-CIRCLES
-    # THEY ARE 2 BY 2 FOR EACH SHAPE(COORDS AND RADIUS FOR SQUARE AND SO ON)
+    # THEY ARE 3 BY 3 FOR EACH SHAPE(COORDS AND RADIUS AND RESET COORDS FOR SQUARE AND SO ON)
     def _chooseCoords(self, side): # randomly chooses some coords that can draw a circle of minimum radius of 60 on the given side
         if self.baseShapeKey == BaseShape.Square.value:
-            return self._chooseSquareCoords(side)
+            return self._chooseCoordsSquareOrRect(side, 60) # minimum 60 radius for squares
         elif self.baseShapeKey == BaseShape.Rect.value:
-            return self._chooseRectCoords(side)
-        else:   
+            return self._chooseCoordsSquareOrRect(side, min(60, self.sides[0] / 2 - 1, self.sides[1] / 2 - 1))
+        else:   # for the rhombus
             return self._chooseRhombCoords(side)
 
-    # this functions resets the "fixed" coords of the circle based on the radius parameter
-    # what I mean by "fixed" coords are the coords that are constant on a given side, ie the y coords on side 1(or side 3)
-    def _resetCoordsSquare(self, sidex, sidey, opsidex, opsidey, side, radius, coords = None):
-        if coords is not None:
-            if side == 1 or side == 3:
-                coords[1] = self.baseShape.rect().y() - radius # keep in mind that the coords of an ellipse(a circle) are the top left coords
-                coords[3] = coords[1] + self.baseShape.rect().height() # of the rectangle that contains it
-            else:
-                coords[0] = self.baseShape.rect().x() - radius
-                coords[2] = coords[0] + self.baseShape.rect().width()
-        else:
-            if side == 1 or side == 3:
-                sidey = self.baseShape.rect().y() - radius
-                opsidey = sidey + self.baseShape.rect().height()
-            else:
-                sidex = self.baseShape.rect().x() - radius
-                opsidex = sidex + self.baseShape.rect().width()
-            return sidex, sidey, opsidex, opsidey
+    # under this are the 3 functions for squares and rects, they are essentially identical
 
-
-    def _chooseSquareCoords(self, side): # this function will return a list of the top-left coords of the rects of the both circles
-        radius = 60 # we are checking for a minimum radius of 60
+    # !!READ THIS!!
+    # this function is the same for both the square and the rect, because the only difference whatsoever between them is the way the radius
+    # is calculated(for the square the minimum radius is 60, while for the rect it can be smaller, depending on the width and height)
+    # this can be easily solved by passing the radius as a parameter
+    def _chooseCoordsSquareOrRect(self, side, radius): # this function will return a list of the top-left coords of the rects of the both circles
         
-        sideX = sideY = opSideX = opSideY = 0 
+        sideX = sideY = opSideX = opSideY = 0 # these are the top left coords of the circle on this side and on the opposite side
         foundGoodCoords = False
         while foundGoodCoords == False:
             foundGoodCoords = True
 
-            # calculates random coordinates that fit on the side(with minimum 60 range)
+            # calculates random coordinates that fit on the side(with minimum radius range)
             if side == 1 or side == 3: # chooses the coordonates, depending on the sides
-                sideX = rand.randrange(int(self.baseShape.rect().x()), int(self.baseShape.rect().x() + self.baseShape.rect().width() - radius * 2)) # * 2 because that's the diameter of the circle
+                sideX = rand.randrange(int(self._baseShape.rect().x()), int(self._baseShape.rect().x() + self._baseShape.rect().width() - radius * 2)) # * 2 because that's the diameter of the circle
                 
                 opSideX = sideX
-                sideY = self.baseShape.rect().y() - radius
-                opSideY = self.baseShape.rect().y() - radius + self.sides[1]
+                sideY = self._baseShape.rect().y() - radius
+                opSideY = self._baseShape.rect().y() - radius + self.sides[1]
             elif side == 2 or side == 4:
-                sideY = rand.randrange(int(self.baseShape.rect().y()), int(self.baseShape.rect().y() + self.baseShape.rect().height() - radius * 2))
+                sideY = rand.randrange(int(self._baseShape.rect().y()), int(self._baseShape.rect().y() + self._baseShape.rect().height() - radius * 2))
                 
                 opSideY = sideY
-                sideX = self.baseShape.rect().x() - radius
-                opSideX = self.baseShape.rect().x() - radius + self.sides[0]
+                sideX = self._baseShape.rect().x() - radius
+                opSideX = self._baseShape.rect().x() - radius + self.sides[0]
 
-            for circle in self.circles: # iterates over all the already drawn semi circles and checks for collision
+            for circle in self._circles: # iterates over all the already drawn semi circles and checks for collision
                 miniCircle, opMiniCircle = self._makeCircle(side, sideX, sideY, opSideX, opSideY, radius)
                 if circle.collidesWithItem(miniCircle) or circle.collidesWithItem(opMiniCircle):
                     foundGoodCoords = False
@@ -169,24 +153,29 @@ class Cell():
         return [sideX, sideY, opSideX, opSideY]
     
     # this function finds the biggest possible radius starting from the given coords
-    def _chooseRadiusSquare(self, side, sidex, sidey, opsidex, opsidey):
+    # just as before, this function would look literally the same for a rectangle
+    def _chooseRadiusSquareOrRect(self, side, sidex, sidey, opsidex, opsidey):
 
-        xPos = self.baseShape.rect().x() # x position of the square
-        height = self.baseShape.rect().height() - 1
-        width = self.baseShape.rect().width() - 1 # minus one because otherwise the circles might end up tangent on the edges, which triggers a collision
-        yPos = self.baseShape.rect().y() # y position of the square
-
+        xPos = self._baseShape.rect().x() # x position of the square
+        height = self._baseShape.rect().height() - 1
+        width = self._baseShape.rect().width() - 1 # minus one because otherwise the circles might end up tangent on the edges, which triggers a collision
+        yPos = self._baseShape.rect().y() # y position of the square
+        minDim = min(height, width) # this variable is only useful when this function is called for a rectangle, the radius should not exceed the smaller value of the two
+        
         maxPosRadNoCircs = 0 # this will hold the maximum possible radius if there were no other circles drawn on the figure
         if side == 1 or side == 3:
             maxPosRadNoCircs = (xPos + width - sidex) / 2 # divides by two because this is technically the biggest diameter
         else:
             maxPosRadNoCircs = (yPos + height - sidey) / 2
 
+        if maxPosRadNoCircs > minDim: # in the case the maximum possible radius is bigger than the minimum dimension(only for rects), than the radius
+            maxPosRadNoCircs = minDim # should be reseted to the value of the minimum dim, so the circle isn't drawn over the bounds of the rectangle
+
         # now it should check whether the current circle intersects other circle that are already drawn, in which case the radius has to be 
         # adjusted accordingly
         actualRadius = maxPosRadNoCircs
-        for circle in self.circles:
-            sidex, sidey, opsidex, opsidey = self._resetCoordsSquare(sidex, sidey, opsidex, opsidey, side, actualRadius) # the top left corner of the ellipse has to be updated every time the radius is changed
+        for circle in self._circles:
+            sidex, sidey, opsidex, opsidey = self._resetCoordsSquareOrRect(sidex, sidey, opsidex, opsidey, side, actualRadius) # the top left corner of the ellipse has to be updated every time the radius is changed
             currCircle, opCircle = self._makeCircle(side, sidex, sidey, opsidex, opsidey, actualRadius)
 
             otherCircCenter = [circle.rect().center().x(), circle.rect().center().y()] # center of the circle variable from the loop
@@ -200,14 +189,29 @@ class Cell():
                 circleCenter = [opCircle.rect().center().x(), opCircle.rect().center().y()]
                 circleDist = math.sqrt((otherCircCenter[0] - circleCenter[0]) ** 2 + (otherCircCenter[1] - circleCenter[1]) ** 2)
                 actualRadius = circleDist - otherCircRadius
-        # here ends this for
+        # here ends this for loop
         return actualRadius
 
-        
+    # this functions resets the "fixed" coords of the circle based on the radius parameter
+    # what I mean by "fixed" coords are the coords that are constant on a given side, ie the y coords on side 1(or side 3)
+    # again, the function for the square and rect is one and the same
+    def _resetCoordsSquareOrRect(self, sidex, sidey, opsidex, opsidey, side, radius, coords = None):
+        if coords is not None:
+            if side == 1 or side == 3:
+                coords[1] = self._baseShape.rect().y() - radius # keep in mind that the coords of an ellipse(a circle) are the top left coords
+                coords[3] = coords[1] + self._baseShape.rect().height() # of the rectangle that contains it
+            else:
+                coords[0] = self._baseShape.rect().x() - radius
+                coords[2] = coords[0] + self._baseShape.rect().width()
+        else:
+            if side == 1 or side == 3:
+                sidey = self._baseShape.rect().y() - radius
+                opsidey = sidey + self._baseShape.rect().height()
+            else:
+                sidex = self._baseShape.rect().x() - radius
+                opsidex = sidex + self._baseShape.rect().width()
+            return sidex, sidey, opsidex, opsidey
 
-
-    def _chooseRectCoords(self, side):
-        opposingSide = side + 2 if side <= 2 else side - 2
 
     def _chooseRhombCoords(self, side):
         opposingSide = side + 2 if side <= 2 else side - 2
@@ -236,8 +240,8 @@ class Cell():
         rhombPoly.append(QtCore.QPointF(500 + np.sin(np.deg2rad(self.angle / 2)) * side, 500 + 2 * np.cos(np.deg2rad(self.angle / 2)) * side))
         rhombPoly.append(QtCore.QPointF(500 + 2 * np.sin(np.deg2rad(self.angle / 2)) * side, 500 + np.cos(np.deg2rad(self.angle / 2)) * side))
         rhombus.setPolygon(rhombPoly)
-        self.baseShape = rhombus
-        scene.addPolygon(self.baseShape)
+        self._baseShape = rhombus
+        scene.addPolygon(self._baseShape)
         return self.sides
 
 
@@ -245,14 +249,14 @@ class Cell():
         self.sides[1] = math.sqrt(self.area) * self._multiplier[0] 
         self.sides[0] = self.area / self.sides[1]
 
-        self.baseShape = QtWidgets.QGraphicsRectItem(500, 500, self.sides[0], self.sides[1])
-        scene.addItem(self.baseShape)
+        self._baseShape = QtWidgets.QGraphicsRectItem(500, 500, self.sides[0], self.sides[1])
+        scene.addItem(self._baseShape)
         return self.sides
 
     def _drawSquare(self, scene, view):
         self.sides[0] = self.sides[1] = math.sqrt(self.area)
 
-        self.baseShape = QtWidgets.QGraphicsRectItem(500, 500, self.sides[0], self.sides[1])
-        scene.addItem(self.baseShape)
+        self._baseShape = QtWidgets.QGraphicsRectItem(500, 500, self.sides[0], self.sides[1])
+        scene.addItem(self._baseShape)
         return self.sides
     
